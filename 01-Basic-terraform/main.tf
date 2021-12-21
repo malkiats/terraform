99a3c52954fc3d89dc1fe9a0c13088d83b710556
malkiats <msjanjua@live.co.uk>
2021-12-21 18:55:34 +0800

    First Basic tf

---
+++ 01-Basic-terraform/main.tf
@@ -0,0 +1,23 @@
+# Settings Block  
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            #version = "~> 3.21" # Optional
+        }
+    }
+}
+
+# Provider
+provider "aws" {
+    profile = "default" #Credential profile
+    region = "ap-southeast-1"
+    #access_key = ""
+    #secret_key = ""
+}
+
+# Resource
+resource "aws_instance" "jenkins" {
+    ami = "ami-0dc5785603ad4ff54"
+    instance_type = "t2.small"
+}
