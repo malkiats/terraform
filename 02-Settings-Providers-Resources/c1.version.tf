0e6cd7df85abfe7afa86e87a8125c08040acd7b4
malkiats <msjanjua@live.co.uk>
2021-12-27 23:11:23 +0800

    updated

--- 02-Settings-Providers-Resources/c1.version.tf
+++ 02-Settings-Providers-Resources/c1.version.tf
@@ -5,7 +5,7 @@ terraform {
     aws = {                     # Local name of your provider
       source = "hashicorp/aws"
       version = "~> 3.0"
-     }
+    }
   }
 }
 
@@ -18,3 +18,9 @@ provider "aws" {
 /*
 AWS Credentials Profile (profile = "default") on local terminal $Home/.aws/credentials
 */
+
+# Resource
+resource "aws_instance" "jenkins" {     // Block type & Block labels & Block inside block
+    ami = "ami-0dc5785603ad4ff54"       // AWS Linux // Argument name & Argument value
+    instance_type = "t2.micro"
+}
