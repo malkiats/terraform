7bb3d0b27736805f3b93aa9aeab5c73a791d004c
Malkiat Singh <43002915+malkiats@users.noreply.github.com>
2023-02-22 22:46:45 +0800

    Create ec2.tf

---
+++ MSLAB/ec2.tf
@@ -0,0 +1,20 @@
+# Resource-8: Create EC2 Instance
+resource "aws_instance" "my-ec2-vm" {
+  ami                    = "ami-0f1a5f5ada0e7da53" # Amazon Linux
+  instance_type          = "t2.micro"
+  key_name               = "terraform-key"
+  subnet_id              = aws_subnet.vpc-mslab-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.mslab-vpc-sg.id]
+  #user_data = file("apache-install.sh")
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install httpd -y
+    sudo systemctl enable httpd
+    sudo systemctl start httpd
+    echo "<h1>Welcome to StackSimplify ! AWS Infra created using Terraform in us-west-2 Region</h1>" > /var/www/html/index.html
+    EOF
+  tags = {
+    "Name" = "myec2vm"
+  }    
+}
